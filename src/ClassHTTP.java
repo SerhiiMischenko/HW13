7e6337d7311a7d2ba0d397e47d4cd86ccd011f44
serhiimischenko <serj.roshen@gmail.com>
2022-12-12 18:40:06 +0200

    Added updateObject(), getUsers(), deleteObject() method(task1)

--- src/ClassHTTP.java
+++ src/ClassHTTP.java
@@ -1,8 +1,10 @@
+import java.io.File;
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.time.Duration;
 
@@ -11,7 +13,10 @@ public class ClassHTTP {
     private static final String TEST_URL = "https://jsonplaceholder.typicode.com/users";
 
     public static void main(String[] args) throws IOException, InterruptedException {
-        createNewObject();
+        //createNewObject();
+        //updateObject();
+        //getUsers();
+        deleteObject();
     }
 
     public static void createNewObject() throws IOException, InterruptedException {
@@ -27,5 +32,46 @@ public class ClassHTTP {
                 client.send(request, HttpResponse.BodyHandlers.ofString());
 
         System.out.println(response.body());
+    }
+
+    public static void updateObject() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(TEST_URL))
+                .timeout(Duration.ofMinutes(1))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofFile(Paths.get("updateUser.json")))
+                .build();
+
+        HttpResponse<String> response =
+                client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+    }
+
+    public static void getUsers() throws IOException, InterruptedException {
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(TEST_URL))
+                .GET()
+                .build();
+
+        HttpResponse<String> response =
+                httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+    }
+
+    public static void deleteObject() throws IOException, InterruptedException {
+        HttpClient httpClient = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(TEST_URL))
+                .DELETE().GET()
+                .build();
+
+        HttpResponse<String> response =
+                httpClient.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.statusCode());
     }
 }
