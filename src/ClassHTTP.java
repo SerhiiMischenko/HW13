2b74a6a98846af8141cade187b4730266384d83d
serhiimischenko <serj.roshen@gmail.com>
2022-12-12 15:58:27 +0200

    Added createNewObject() method (task1)

--- src/ClassHTTP.java
+++ src/ClassHTTP.java
@@ -1,3 +1,31 @@
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.file.Paths;
+import java.time.Duration;
+
 public class ClassHTTP {
 
+    private static final String TEST_URL = "https://jsonplaceholder.typicode.com/users";
+
+    public static void main(String[] args) throws IOException, InterruptedException {
+        createNewObject();
+    }
+
+    public static void createNewObject() throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(TEST_URL))
+                .timeout(Duration.ofMinutes(1))
+                .header("Content-Type", "application/json")
+                .POST(HttpRequest.BodyPublishers.ofFile(Paths.get("user.json")))
+                .build();
+
+        HttpResponse<String> response =
+                client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        System.out.println(response.body());
+    }
 }
