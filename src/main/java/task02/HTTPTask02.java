e01034d172bb210bb2424612650ae03dc034124c
serhiimischenko <serj.roshen@gmail.com>
2022-12-13 16:49:58 +0200

    Rework getCommentById() method(task2)

--- src/main/java/task02/HTTPTask02.java
+++ src/main/java/task02/HTTPTask02.java
@@ -1,6 +1,27 @@
 package task02;
 
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import org.jsoup.Jsoup;
+
+import java.io.File;
+import java.io.IOException;
+
 public class HTTPTask02 {
 
-
+    public static void main(String[] args) throws IOException {
+        getCommentById();
+    }
+    public static void getCommentById() throws IOException {
+        String document = Jsoup.connect("https://jsonplaceholder.typicode.com/posts/10/comments")
+                .ignoreContentType(true).execute().body();
+        JsonArray a = new JsonParser().parse(document).getAsJsonArray();
+        JsonElement lastComment = a.get(a.size() -1);
+        JsonObject jo = (JsonObject) lastComment;
+        String postId = String.valueOf(jo.get("postId"));
+        String id = String.valueOf(jo.get("id"));
+        String body = String.valueOf(jo.get("body"));
+    }
 }
